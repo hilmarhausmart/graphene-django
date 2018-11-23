97616de3ed68373e410ec3b90921eab2f7d6f14b
Hilmar Ævar Hilmarsson <hilmar@hausmart.com>
2018-11-23 11:10:17 +0000

    Permissions for mutations

--- graphene_django/mutation.py
+++ graphene_django/mutation.py
@@ -1,5 +1,9 @@
+import warnings
+
 from graphene.types.mutation import Mutation
 from graphene.relay.mutation import ClientIDMutation
+
+from rest_framework.exceptions import PermissionDenied
 
 from .fields import DjangoField
 
@@ -12,7 +16,27 @@ class DjangoClientIDMutation(ClientIDMutation):
 
 
     @classmethod
-    def mutate(cls, root, info, input):
+    def mutate(cls, root, info, input, permission_classes=None):
+        if not permission_classes:
+            if hasattr(info, "context") and info.context and info.context.get("view", None):
+                permission_classes = info.context.get(
+                    "view"
+                ).resolver_permission_classes
+            else:
+                warnings.warn(
+                    UserWarning(
+                        "DjangoNodeField should not be called without context."
+                    )
+                )
+
+        if permission_classes:
+            for permission in [p() for p in permission_classes]:
+                if not permission.has_permission(
+                    info.context.get("request"), info.context.get("view")
+                ):
+                    raise PermissionDenied(detail=getattr(permission, "message", None))
+
+
         return super(DjangoClientIDMutation, cls).mutate(root, info, input)
 
     @classmethod
